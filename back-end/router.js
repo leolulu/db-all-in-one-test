e1ee5378ac27924215251349166095608b45aa5d
leolulu <348699103@qq.com>
2018-08-21 23:07:44 +0800

    post处理

--- back-end/router.js
+++ back-end/router.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql')
 var cors = require('cors')
+var bodyParser = require('body-parser')
 
 router.use(cors())
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 
 var pool = mysql.createPool({
   connectionLimit: 10,
@@ -22,7 +25,13 @@ router.get('/user', function (req, res) {
     if (error) return console.log('卧槽他妈的出错了:', error);
     res.end(JSON.stringify(results))
   });
+})
 
+router.post('/posttest',function(req,res){
+  console.log('收到post请求')
+  console.log(req.body)
+  res.send(req.body)
+  res.end()
 })
 
 module.exports = router;
--- front-end/src/components/maincontainer.vue
+++ front-end/src/components/maincontainer.vue
@@ -1,27 +1,30 @@
 <template>
-    <div>
-        <el-container>
-            <el-header class="elhead">
-                <el-button icon="el-icon-search" circle @click="get" class="head-logo"></el-button>
-            </el-header>
-            <el-container class="mymian">
-                <el-aside class="elaside" width='250px'>
-                    <el-menu>
-                        <el-menu-item :index="index+'i'" v-for='(i,index) in menuid' :key='index'>选项{{i}}</el-menu-item>
-                    </el-menu>
-                </el-aside>
-                <el-main class="elmain">
-                    <el-table :data="tabledata">
-                        <el-table-column prop="id" label="ID"></el-table-column>
-                        <el-table-column prop="name" label="姓名"></el-table-column>
-                        <el-table-column prop="url" label="网址"></el-table-column>
-                        <el-table-column prop="country" label="国家"></el-table-column>
-                        <el-table-column prop="alexa" label="AXELA"></el-table-column>
-                    </el-table>
-                </el-main>
-            </el-container>
-        </el-container>
-    </div>
+  <div>
+    <el-container>
+      <el-header class="elhead">
+        <el-button-group>
+          <el-button icon="el-icon-search" circle @click="get" class="head-logo"></el-button>
+          <el-button icon="el-icon-bell" @click='post'>POST</el-button>
+        </el-button-group>
+      </el-header>
+      <el-container class="mymian">
+        <el-aside class="elaside" width='250px'>
+          <el-menu>
+            <el-menu-item :index="index+'i'" v-for='(i,index) in menuid' :key='index'>选项{{i}}</el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main class="elmain">
+          <el-table :data="tabledata">
+            <el-table-column prop="id" label="ID"></el-table-column>
+            <el-table-column prop="name" label="姓名"></el-table-column>
+            <el-table-column prop="url" label="网址"></el-table-column>
+            <el-table-column prop="country" label="国家"></el-table-column>
+            <el-table-column prop="alexa" label="AXELA"></el-table-column>
+          </el-table>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
@@ -43,6 +46,18 @@ export default {
           this.tabledata = res.data;
         })
         .catch(err => console.log("eooro: ", err));
+    },
+    post() {
+      axios
+        .post(
+          "http://127.0.0.1:3000/posttest?name=leo&shoe[color]=blue&shoe[type]=shit",
+          {
+            firstName: "Fred",
+            lastName: "Flintstone"
+          }
+        )
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
     }
   }
 };
